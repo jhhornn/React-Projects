9c80bead7a69e76f1f1f953800c3d956d8ffc257
Awosise Oluwaseun <awosiseo@gmail.com>
2022-08-07 22:40:03 +0100

    card component setup

---
+++ digital business card/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import twitter from "../assets/Twitter Icon.svg";
+import facebook from "../assets/Facebook Icon.svg";
+import instagram from "../assets/Instagram Icon.svg";
+import github from "../assets/Github Icon.svg";
+
+export default function Footer() {
+    return (
+        <>
+            <div className="footer--icons">
+                <img src={twitter} alt="" />
+                <img src={facebook} alt="" />
+                <img src={instagram} alt="" />
+                <img src={github} alt="" />
+            </div>
+        </>
+    )
+}
---
+++ digital business card/src/components/Interests.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Interests() {
+    return (
+        <>
+            <h2>Interests</h2>
+            <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+        </>
+    )
+}

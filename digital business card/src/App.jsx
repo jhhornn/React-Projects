01395d3a42f6cddb848da54700d1082c969b8dcd
Awosise Oluwaseun <awosiseo@gmail.com>
2022-08-08 00:50:45 +0100

    partial css

--- digital business card/src/App.jsx
+++ digital business card/src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import './App.css'
+import Info from './components/Info.jsx'
+import About from './components/About.jsx'
+import Interests from './components/Interests.jsx'
+import Footer from './components/Footer.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+      <main className='main--content'>
+        <Info />
+        <About />
+        <Interests />
+      </main>
+      <Footer />
     </>
   )
 }
---
+++ digital business card/src/components/Info.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import image from "../assets/me.png"
+import mail from "../assets/Mail.svg"
+import linkedin from "../assets/linkedin.svg"
+
+export default function Info() {
+    return (
+        <>
+            <img src={image} className="info--image" alt="my picture" />
+            <h1 className="my--name">Awosise Oluwaseun</h1>
+            <h3 className="my--role">Fullstack Developer</h3>
+            <h4 className="my--site">oluwaseun.website</h4>
+            <div className="buttons flex">
+                <a href="#" className="btn email"><img src={mail} alt="mail" className="intro--icon mail" />Email</a>
+                <a href="#" className="btn linkedin"><img src={linkedin} alt="linkedin" className="intro--icon" />LinkedIn</a>
+            </div>
+        </>
+    )
+}
